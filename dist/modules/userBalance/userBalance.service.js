fdaa726adfd067a99a9acb551004a45082934f87
aabao325 <62531785+aabao325@users.noreply.github.com>
2025-02-17 12:19:20 +0800

    Update userBalance.service.js

--- dist/modules/userBalance/userBalance.service.js
+++ dist/modules/userBalance/userBalance.service.js
@@ -237,15 +237,6 @@ let UserBalanceService = class UserBalanceService {
             newNonMemberBalance = Math.max(b[nonMember] - remainingAmount, 0);
             remainingAmount -= b[nonMember] - newNonMemberBalance;
         }
-
-        // 在计算所有需要的扣费后检查会员状态
-        const now = new Date();
-        const isMemberActive = b.packageId && b.expirationTime && new Date(b.expirationTime) > now;
-
-        if (isMemberActive && (deductionType === 1 || deductionType === 2)) {
-            console.log(`会员用户对话，用户ID: ${userId}，用户名: ${user.username || '未知用户'}，会员有效期未到期，不扣积分。`);
-            return;
-        }
         
         const updateBalance = {
             [member]: newMemberBalance,
